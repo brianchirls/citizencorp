af0efb9b6ac2daaffdc4d589f1bf963b1d6bf0fb
Brian Chirls <brian@chirls.com>
2012-06-20 13:17:28 -0400

    Added influence explorer plugin

---
+++ js/plugins/popcorn.influence-explorer.js
@@ -0,0 +1,18 @@
+(function (Popcorn) {
+
+"use strict";
+
+	Popcorn.basePlugin( 'influenceExplorer' , function(options, base) {
+		var popcorn = this,
+			media = popcorn.media;
+
+		return {
+			start: function( event, options ) {
+			},
+			end: function( event, options ) {
+			},
+			_teardown: function( options ) {
+			}
+		};
+	});
+})( Popcorn );
