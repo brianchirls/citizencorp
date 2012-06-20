90e213a86002aadf956e4d660e131adb3cd70a2f
Brian Chirls <brian@chirls.com>
2012-06-20 13:12:13 -0400

    put script.js in a closure, add frameAnimation option

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-var popcorn = Popcorn('#video');
+(function (window, undefined) {
+	var Popcorn = window.Popcorn,
+		document = window.document,
 
+		popcorn;
+
+	popcorn = Popcorn('#video', {
+		frameAnimation: true
+	});
+
+
+
+/*
+	popcorn.defaults.influenceExplorer = {
+		state: 'MD'
+	};
+
+	popcorn.influenceExplorer({
+		start: 1,
+		end: 2
+		//state: 'MD'
+	});
+*/
+}(window));
